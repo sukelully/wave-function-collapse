94cc4b0e100682b521103bc6fbfe2996c3cc4ed3
Luke Sullivan <sully097@gmail.com>
2025-03-01 01:11:58 +0000

    begin entropy sorting

--- sketch.js
+++ sketch.js
@@ -1,6 +1,16 @@
 const tiles = [];
 
-function preLoad() {
+const grid = [];
+
+const DIM = 2;
+
+const BLANK = 0;
+const UP = 1;
+const RIGHT = 2;
+const DOWN = 3;
+const LEFT = 4;
+
+function preload() {
     tiles[0] = loadImage("tiles/blank.png");
     tiles[1] = loadImage("tiles/up.png");
     tiles[2] = loadImage("tiles/right.png");
@@ -8,15 +18,46 @@ function preLoad() {
     tiles[4] = loadImage("tiles/left.png");
 }
 
-function setUp() {
+function setup() {
     createCanvas(400, 400);
+
+    for (let i = 0; i < DIM * DIM; i++) {
+        grid[i] = {
+            collapsed: false,
+            options: [BLANK, UP, RIGHT, DOWN, LEFT]
+        };
+    }
+
+    grid[2].options = [BLANK, UP]
 }
 
 function draw() {
     background(0);
-    Image(tiles[0], 0, 0);
-    Image(tiles[1], 50, 0);
-    Image(tiles[2], 100, 0);
-    Image(tiles[4], 150, 0);
-    Image(tiles[5], 0, 0);
+
+    // Pick cell with least entropy
+    const gridCopy = grid.slice();
+    gridCopy.sort((a, b) => {
+        return a.options.length - b.options.length;
+    });
+    console.log(grid);
+    console.log(gridCopy);
+    
+    const w = width / DIM;
+    const h = height / DIM;
+
+    for (let j = 0; j < DIM; j++) {
+        for (let i = 0; i < DIM; i++) {
+            let cell = grid[i + j * DIM];
+            if (cell.collapsed) {
+                let index = cell.options[0];
+                image(tiles[index], i * w, j * h, w, h);
+            } else {
+                fill(255);
+                // stroke(255);
+                rect(i * w, j * h, w, h);
+            }
+        }
+    }
+
+    noLoop();
 }
